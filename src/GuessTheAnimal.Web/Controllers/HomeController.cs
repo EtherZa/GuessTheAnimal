63c77983d046429a6db85179ace491ff08af4dbc
EtherZa <richardpringle+git@gmail.com>
2017-11-22 02:41:04 +0200

    Initial structure

---
+++ src/GuessTheAnimal.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace GuessTheAnimal.Web.Controllers
+{
+    using System.Diagnostics;
+
+    using GuessTheAnimal.Web.Models;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Error()
+        {
+            return this.View(
+                new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
